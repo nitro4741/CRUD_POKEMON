546c32c75d02dd9d76afa3260c52257b35912404
DiegoR30 <die-35_45rhotmail.com>
2024-01-15 20:12:20 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>POKEMON_CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../assets/css/all.min.css" rel="stylesheet">
+  </head>
+
+
+  <body class="body">
+    <nav class="navbar bg-primary bg-gradient shadow-lg  rounded">
+      <div class="container-fluid">
+        <a  href="#"><img src="./media/logo-pokemon.png" alt="Logo" width="180"  class="d-inline-block align-text-top img-thumbnail>"></a>
+        <h2 class="text-light">POKEMON CRUD</h2>
+      </div>
+    </nav>
+    <div class="d-grid gap-5 col-6 mx-auto mx-auto p-3" style="width: 250px;">
+      <button class="btn btn-lg shadow  bg-warning bg-gradient fw-semibold" type="button">CAPTURA UN POKEMON</button>
+    </div>
+    <table class="table table-sm table-striped table-hover mt-4 container-xl">
+            <thead class="table-warning ">
+                <tr>
+                    <th>#</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Género</th>
+                    <th>Poster</th>
+                    <th>Acción</th>
+                </tr>
+                <!-- <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><img src="" width="100"></td>
+                        <td>
+                            <a href="#"> Editar</a>
+                            <a href="#">Eliminar</a>
+                        </td>
+                    </tr>
+                </tbody> -->
+            </thead>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
